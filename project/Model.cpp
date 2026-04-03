9896be7931c7ed0cd7dcf6950b853c9aaa17292b
akimoto <k024g0095@g.neec.ac.jp>
2026-04-03 16:37:19 +0900

    modelの途中

--- project/Model.cpp
+++ project/Model.cpp
@@ -1,1 +1,120 @@
 #include "Model.h"
+#include <cassert>
+//ファイルやディレクトリに関する操作を行うライブラリ
+#include <filesystem>
+//ファイルに書いたり読んだりするライブラリ
+#include<fstream>
+
+using namespace MyMath;
+
+void Model::Initialize(ModelCommon* modelCommon)
+{
+	modelCommon_ = modelCommon;
+	modelData = LoadObjFile("resources", "plane.obj");
+
+}
+
+Model::MaterialData Model::LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename)
+{
+	MaterialData materialData;
+	std::string line;
+	std::ifstream file(directoryPath + "/" + filename);
+	assert(file.is_open());
+
+	while (std::getline(file, line))
+	{
+		std::string identifier;
+		std::istringstream s(line);
+		s >> identifier;
+
+		if (identifier == "map_Kd")
+		{
+			std::string textureFilename;
+			s >> textureFilename;
+
+			materialData.textureFilePath = directoryPath + "/" + textureFilename;
+		}
+	}
+
+	return materialData;
+}
+
+Model::ModelData Model::LoadObjFile(const std::string& directoryPath, const std::string& filename)
+{
+	ModelData modelData;
+	std::vector<Vector4> positions;
+	std::vector<Vector3> normals;
+	std::vector<Vector2> texcoords;
+	std::string line;
+
+	std::ifstream file(directoryPath + "/" + filename);
+	assert(file.is_open());
+
+	while (std::getline(file, line))
+	{
+		std::string identifier;
+		std::istringstream s(line);
+		s >> identifier;
+
+		if (identifier == "v")
+		{
+			Vector4 position;
+			s >> position.x >> position.y >> position.z;
+			position.w = 1.0f;
+			positions.push_back(position);
+		}
+		else if (identifier == "vt")
+		{
+			Vector2 texcoord;
+			s >> texcoord.x >> texcoord.y;
+			texcoords.push_back(texcoord);
+		}
+		else if (identifier == "vn")
+		{
+			Vector3 normal;
+			s >> normal.x >> normal.y >> normal.z;
+			normals.push_back(normal);
+		}
+		else if (identifier == "f")
+		{
+			VertexData triangle[3];
+			for (int32_t faceVertex = 0; faceVertex < 3; ++faceVertex)
+			{
+				std::string vertexDefinition;
+				s >> vertexDefinition;
+
+				std::istringstream v(vertexDefinition);
+				uint32_t elementIndices[3];
+				for (int32_t element = 0; element < 3; ++element)
+				{
+					std::string index;
+					std::getline(v, index, '/');
+					elementIndices[element] = std::stoi(index);
+				}
+				Vector4 position = positions[elementIndices[0] - 1];
+				position.x *= -1.0f;
+				Vector2 texcoord = texcoords[elementIndices[1] - 1];
+				texcoord.y = 1.0f - texcoord.y;
+				Vector3 normal = normals[elementIndices[2] - 1];
+				//normal.x *= -1.0f;
+				//VertexData vertex = { position,texcoord,normal };
+				//VertexData vertex = { position,texcoord };
+				//modelData.vertices.push_back(vertex);
+				triangle[faceVertex] = { position,texcoord,normal };
+				//triangle[faceVertex] = { position,texcoord };
+			}
+			modelData.vertices.push_back(triangle[2]);
+			modelData.vertices.push_back(triangle[1]);
+			modelData.vertices.push_back(triangle[0]);
+		}
+		else if (identifier == "mtllib")
+		{
+			std::string materialFilename;
+			s >> materialFilename;
+
+			modelData.material = LoadMaterialTemplateFile(directoryPath, materialFilename);
+		}
+	}
+
+	return modelData;
+}
--- project/Model.h
+++ project/Model.h
@@ -1,5 +1,65 @@
 #pragma once
+#include "ModelCommon.h"
+#include "Logger.h"
+#include "StringUtility.h"
+#include "MyMath.h"
+#include "TextureManager.h"
+
+#include <stdint.h>
+
 class Model
 {
+	struct VertexData
+	{
+		Vector4 position;
+		Vector2 texcoord;
+		Vector3 normal;
+	};
+
+	struct MaterialData
+	{
+		std::string textureFilePath;
+		uint32_t textureIndex = 0;
+	};
+
+	struct ModelData
+	{
+		std::vector<VertexData> vertices;
+		MaterialData material;
+	};
+
+	struct Material
+	{
+		MyMath::Vector4 color;
+		int32_t enableLighting;
+		float padding[3];
+		MyMath::Matrix4x4 uvTransform;
+	};
+
+public:
+
+	void Initialize(ModelCommon* modelCommon);
+
+	static MaterialData LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename);
+
+	static ModelData LoadObjFile(const std::string& directoryPath, const std::string& filename);
+
+
+private:
+
+	// モデルポインタ
+	ModelCommon* modelCommon_;
+	// Objファイルのデータ
+	ModelData modelData;
+	// 
+	Microsoft::WRL::ComPtr<ID3D12Resource> vertexResource = nullptr;
+	//
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferView;
+	//
+	VertexData* vertexData = nullptr;
+	//
+	Microsoft::WRL::ComPtr<ID3D12Resource> materialResource = nullptr;
+	//
+	Material* materialData = nullptr;
 };
 
